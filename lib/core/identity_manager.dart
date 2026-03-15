8ff1f103a87f1eeb0689bae4239964ee01725b76
akinik <akinik456@gmail.com>
2026-03-15 16:49:16 +0300

    feat(identity): add locatorId generator

--- lib/core/identity_manager.dart
+++ lib/core/identity_manager.dart
@@ -12,4 +12,16 @@ class IdentityManager {
     await prefs.setString('requesterId', id);
     return id;
   }
+
+	static Future<String> getLocatorId() async {
+	  final prefs = await SharedPreferences.getInstance();
+
+	  var id = prefs.getString('locatorId');
+	  if (id != null && id.isNotEmpty) return id;
+
+	  id = const Uuid().v4();
+	  await prefs.setString('locatorId', id);
+	  return id;
+	}  
+  
 }
